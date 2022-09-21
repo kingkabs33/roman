d93bcd42e20ff2daa87832490e934864ead4df76
kingkabs33 <mj.kabuya@gmail.com>
2022-09-21 13:00:19 +0100

    test until 3000

--- test/src/Main.java
+++ test/src/Main.java
@@ -2,6 +2,13 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Main {
+    public static void main(String args[])
+    {
+        for (int i = 1;i<3000;i++)
+        {
+            System.out.println("i="+i+" -> "+RomanNumerals(i));
+        }
+    }
 
     public static String RomanNumerals(int num) {
         LinkedHashMap<String, Integer> romanNumerals = new LinkedHashMap();
@@ -19,7 +26,25 @@ public class Main {
         romanNumerals.put("IV", 4);
         romanNumerals.put("I", 1);
         String result = "";
-
+        for(Map.Entry<String, Integer> numeral : romanNumerals.entrySet())
+        {
+            int matches = num/numeral.getValue();
+            result = result+repeat(numeral.getKey(), matches);
+            num = num % numeral.getValue();
+        }
         return result;
+    }
+    public static String repeat(String s, int n)
+    {
+        if(s == null)
+        {
+            return null;
+        }
+        final StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < n; i++)
+        {
+            sb.append(s);
+        }
+        return sb.toString();
     }
 }
